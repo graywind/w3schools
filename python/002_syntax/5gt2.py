20f1d855d51b183b3eaf43e0fcf9d35dd9d97d51
Graywind <hewhocommits@clbuttic.dev>
2023-12-11 09:46:09 -0800

    Syntax and comments

---
+++ python/002_syntax/5gt2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+if 5 > 2:
+ print("Five is greater than two!")
+if 5 > 2:
+    print("Five is greater than two!")
---
+++ python/003_comments/helloworld.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#This is a comment
+print("Hello, World!") #This is also a comment
+"""
+Doing the needful
+One keystoke at a time
+"""
