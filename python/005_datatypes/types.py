651b3db14bf649e0a0e580c2f4d378fbefc1cea5
Graywind <hewhocommits@clbuttic.dev>
2023-12-13 11:26:29 -0800

    005-datatypes

---
+++ python/005_datatypes/types.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+Section Name: Data Types
+URL: https://www.w3schools.com/python/python_datatypes.asp
+"""
+#str
+x = "Hello World"
+print(x)
+print(type(x))
+
+#int
+x = 20
+print(x)
+print(type(x))
+
+#float
+x = 20.5
+print(x)
+print(type(x))
+
+#complex
+x = 1j
+print(x)
+print(type(x))
+
+#list
+x = ["apple","banana","cherry"]
+print(x)
+print(type(x))
+
+#tuple
+x = ("apple","banana","cherry")
+print(x)
+print(type(x))
+
+#range
+x = range(6)
+print(x)
+print(type(x))
+
+#dict
+x = {"name" : "John", "age" : 36}
+print(x)
+print(type(x))
+
+#set
+x = {"apple","banana","cherry"}
+print(x)
+print(type(x))
+
+#frozenset
+x = frozenset({"apple","banana","cherry"})
+print(x)
+print(type(x))
+
+#boolean
+x = True
+print(x)
+print(type(x))
+
+#bytes
+x = b"Hello"
+print(x)
+print(type(x))
+
+#bytearray
+x = bytearray(5)
+print(x)
+print(type(x))
+
+#memoryview
+x = memoryview(bytes(5))
+print(x)
+print(type(x))
+
+#NoneType
+x = None
+print(x)
+print(type(x))
+
