0dc9a9078a8a4ca01a7f69763bf69e620c9cbbf3
Graywind <hewhocommits@clbuttic.dev>
2023-12-14 08:25:11 -0800

    007-casting

---
+++ python/007_casting/casts.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Section Name: Casting
+URL: https://www.w3schools.com/python/python_casting.asp
+"""
+x = int(1)    # x will be 1
+y = int(2.8)  # y will be 2
+z = int("3")  # z will be 3
+
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+
+x = float(1)      # x will be 1.0
+y = float(2.8)    # y will be 2.8
+z = float("3")    # z will be 3.0
+w = float("4.2")  # w will be 4.2
+
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+print(w)
+print(type(w))
+
+x = str("s1")  # x will be 's1'
+y = str(2)     # y will be '2'
+z = str(3.0)   # z will be '3.0'
+
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
